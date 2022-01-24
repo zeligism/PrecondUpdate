5f93f7def1b8fba9bae18a600368b88acf259a9f
Abdulla Al Mansoori <abdulla.almansoori@nyu.edu>
2022-01-24 14:27:38 +0400

    fixed download script

--- download_datasets.sh
+++ download_datasets.sh
@@ -1,7 +1,7 @@
 DATASETS=("a1a")
 DATASETS_DIR="datasets"
 mkdir -p "${DATASETS_DIR}"
-BASE_LINK="https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/"
+BASE_LINK="https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary"
 for dataset in "${DATASETS[@]}"
-    do [[ ! -f "${dataset}" ]] && wget -O "${DATASETS_DIR}/${dataset}" "${BASE_LINK}/${dataset}_scale"
+    do [[ ! -f "${dataset}" ]] && wget -O "${DATASETS_DIR}/${dataset}" "${BASE_LINK}/${dataset}"
 done
