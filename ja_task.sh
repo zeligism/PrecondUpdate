b6f72b5ca8b23defd89af0fdc2972d5ce33e3052
zeligism <abdulla.almansoori@mbzuai.ac.ae>
2022-01-25 16:32:29 +0400

    added hack for running job arrays without exiting prematurely

--- ja_task.sh
+++ ja_task.sh
@@ -3,8 +3,15 @@
 #SBATCH --output="%x-%A_%a.out"
 #SBATCH --ntasks=1
 #SBATCH --time=0:10:00
+
+completedjobs=".completedjobs"
+sync_wait=10
+# Note: this should execute before any other job finishes
+rm -f $completedjobs
+
 pwd; hostname; date
 source activate opt # assuming conda env 'opt' exists
+
 
 JA_FILE="${JA_FILE:-"experiment.ja"}"
 LAST_JOB=$(wc -l < ${JA_FILE})
@@ -26,7 +33,10 @@ for (( run=$START; run<=END; run++ )); do
 done
 
 # hack to make sure all jobs finish before job scripts finishes
-# assuming all jobs are running the same number of commands
-# this is because there seems to be a glitch in he current SLURM system
+# this is because there seems to be a glitch in the current SLURM system
 # that kills the remaining jobs in the array as soon as a single job finishes
-sleep 30  # sleep for half a minute
+echo ${SLURM_ARRAY_TASK_ID} >> $completedjobs
+while (( $(wc -l < $completedjobs) < ${SLURM_ARRAY_TASK_COUNT} )); do
+	sleep ${sync_wait}
+done
+
