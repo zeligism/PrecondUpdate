69a13b97d6835d84fbd5f15e379772e453602096
Abdulla Al Mansoori <abdulla.almansoori@nyu.edu>
2022-01-26 22:11:39 +0400

    added D initialization (estimate with 100 samples in case we use hutchinson's method)

--- src/optimizer.py
+++ src/optimizer.py
@@ -2,11 +2,11 @@
 from loss import F, grad, hessian, hvp
 import numpy as np
 
-def collect_data(X,y,w,lam=0.0):
+def collect_data(ep,X,y,w,lam=0.0):
     loss = F(X,y,w)
     g_norm = np.linalg.norm(grad(X,y,w))**2
     error = np.mean(X.dot(w)*y < 0)
-    return (loss, g_norm, error)
+    return (ep, loss, g_norm, error)
 
 def sample_z(size):
     return 2 * np.random.randint(0,2,size) - 1
@@ -14,17 +14,32 @@ def sample_z(size):
 def norm_scaled(x,D):
     return np.sqrt(np.sum(x * D * x))
 
+def initialize_D(X,y,w, precond="hutchinson", N=100):
+    if precond == "hessian":
+        return np.diagonal(hessian(X,y,w)) + 1e-10
+    elif precond == "hutchinson":
+        # grad(w+z) - grad(w) approximates H(w)z
+        D = 0.
+        for _ in range(N):
+            z = sample_z()
+            D += z * (grad(X,y,w+z) - grad(X,y,w)) / N
+        return D
+    else:
+        return 1.
+
 
 def OASIS(X, y, T=10000, BS=1, gamma=1.0, beta=0.99, lam=0.0, alpha=1e-5):
     data = []
-    w = np.zeros(X.shape[1])
-    D = np.ones_like(w)
     theta = 1e10
+    w = np.zeros(X.shape[1])
+    D = initialize_D(X,y,w) + lam
+
     # first step
     i = np.random.choice(X.shape[0], BS)
     g = grad(X,y,w,i,lam=lam)
     w_prev = w[:]
     w = w - gamma * D**-1 * g
+
     for it in range(T):
         i = np.random.choice(X.shape[0], BS)
         # Calculate gradients
@@ -53,7 +68,7 @@ def OASIS(X, y, T=10000, BS=1, gamma=1.0, beta=0.99, lam=0.0, alpha=1e-5):
 def SGD(X, y, T=10000, BS=1, gamma=0.0002, beta=0.999, lam=0.0, alpha=1e-5, precond="hutchinson"):
     data = []
     w = np.zeros(X.shape[1])
-    D = 1.
+    D = initialize_D(X,y,w) + lam
     for it in range(T):
         # Calculate gradients
         i = np.random.choice(X.shape[0], BS)
@@ -66,7 +81,7 @@ def SGD(X, y, T=10000, BS=1, gamma=0.0002, beta=0.999, lam=0.0, alpha=1e-5, prec
         elif precond == "hutchinson":
             # estimate hessian diagonal
             z = sample_z(w.shape)
-            j = i #np.random.choice(X.shape[0], BS)
+            j = i
             #D_est = z * hvp(X,y,w,z,j) + lam  # why is this bad
             D_est = z * (grad(X,y,w+z,j,lam=lam) - g) + lam
             D = np.abs(beta * D + (1-beta) * D_est)
@@ -84,7 +99,7 @@ def SGD(X, y, T=10000, BS=1, gamma=0.0002, beta=0.999, lam=0.0, alpha=1e-5, prec
 def SARAH(X, y, T=10, BS=1, gamma=0.2, beta=0.999, lam=0.0, alpha=1e-5, precond="hutchinson"):
     data = []
     wn = np.zeros(X.shape[1])
-    D = 1.
+    D = initialize_D(X,y,w) + lam
     for ep in range(T):
         v = grad(X,y,wn,lam=lam)
         nv0 = np.linalg.norm(v)
@@ -104,7 +119,7 @@ def SARAH(X, y, T=10, BS=1, gamma=0.2, beta=0.999, lam=0.0, alpha=1e-5, precond=
             elif precond == "hutchinson":
                 # estimate hessian diagonal
                 z = sample_z(wn.shape)
-                j = i #np.random.choice(X.shape[0], BS)
+                j = i
                 #D_est = z * hvp(X,y,wn,z,j) + lam  # why is this bad
                 D_est = z * (grad(X,y,wn+z,j,lam=lam) - gn) + lam
                 D = np.abs(beta * D + (1-beta) * D_est)
@@ -127,7 +142,7 @@ def SARAH(X, y, T=10, BS=1, gamma=0.2, beta=0.999, lam=0.0, alpha=1e-5, precond=
 def SVRG(X, y, T=10, BS=1, gamma=0.2, beta=0.999, lam=0.0, alpha=1e-5, precond=None):
     data = []
     w_out = np.zeros(X.shape[1])
-    D = 1.
+    D = initialize_D(X,y,w) + lam
     for ep in range(T):
         g_full = grad(X,y,w_out,lam=lam)
         gnorm0 = np.linalg.norm(g_full)
@@ -146,7 +161,7 @@ def SVRG(X, y, T=10, BS=1, gamma=0.2, beta=0.999, lam=0.0, alpha=1e-5, precond=N
             elif precond == "hutchinson":
                 # estimate hessian diagonal
                 z = sample_z(w_in.shape)
-                j = i #np.random.choice(X.shape[0], BS)
+                j = i
                 #D_est = z * hvp(X,y,wn,z,j) + lam  # why is this bad
                 D_est = z * (grad(X,y,w_in+z,j,lam=lam) - g_in) + lam
                 D = np.abs(beta * D + (1-beta) * D_est)
