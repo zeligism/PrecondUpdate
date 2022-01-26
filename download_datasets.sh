085f619d83f56baff1f2ac41843e4caec676bb1d
Abdulla Al Mansoori <abdulla.almansoori@nyu.edu>
2022-01-26 21:49:38 +0400

    added more datasets

--- download_datasets.sh
+++ download_datasets.sh
@@ -1,7 +1,17 @@
-DATASETS=("a1a")
+DATASETS=("a1a" "a9a" "rcv1" "covtype" "real-sim" "w8a" "ijcnn1" "news20")
 DATASETS_DIR="datasets"
 mkdir -p "${DATASETS_DIR}"
 BASE_LINK="https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary"
-for dataset in "${DATASETS[@]}"
-    do [[ ! -f "${dataset}" ]] && wget -O "${DATASETS_DIR}/${dataset}" "${BASE_LINK}/${dataset}"
+for dataset in "${DATASETS[@]}"; do
+    if [[ $dataset == "rcv1" ]]; then
+        LINK="${BASE_LINK}/rcv1_train.binary"
+    elif [[ $dataset == "covtype" ]]; then
+        LINK="${BASE_LINK}/covtype.libsvm.binary"
+    elif [[ $dataset == "news20" ]]; then
+        LINK="${BASE_LINK}/news20.binary"
+    else
+        LINK="${BASE_LINK}/${dataset}"
+    fi
+    dataset_path="${DATASETS_DIR}/${dataset}"
+    [[ ! -f "${dataset_path}" ]] && wget -O "${dataset_path}" "$LINK"
 done
