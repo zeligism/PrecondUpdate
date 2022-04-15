78ea87f17a63ea7dcc88531cbbb0c446619f5da7
Abdulla Almansoori <abdulla.almansoori@mbzuai.ac.ae>
2022-04-15 17:21:24 +0400

    rewrote hyperparam grid and arg naming and edge case handling

--- src/run_experiment.py
+++ src/run_experiment.py
@@ -7,45 +7,23 @@ from itertools import product
 from random import random
 from train import *
 
-LOG_DIR = "logs_new_combined+adam"
-
-T = 50
-SEEDS = (123,124,125)
-#DATASETS = ("covtype", "ijcnn1", "news20", "rcv1",)
-DATASETS = ("a9a", "w8a", "rcv1", "real-sim")
-OPTIMIZERS = ("SGD", "Adam", "SARAH", "L-SVRG")
-#BATCH_SIZES = (128, 512, 2048)
-BATCH_SIZES = (128,)
-GAMMAS = (2**i for i in range(-16, 3, 2))
-LAMBDAS = (0.0,)
-PS = (0.99,)
-PRECONDS = (None, "hutchinson")
-BETAS = (0.999,)
-ALPHAS = (1e-1, 1e-3, 1e-5, 1e-7)
-CORRUPT = (None, [-3,0], [0,3], [-3,3])
 
 DRY_RUN = False
-
-
-HYPERPARAM_GRID = product(SEEDS,
-                          DATASETS,
-                          OPTIMIZERS,
-                          BATCH_SIZES,
-                          GAMMAS,
-                          LAMBDAS,
-                          PS,
-                          PRECONDS,
-                          BETAS,
-                          ALPHAS,
-                          CORRUPT,
-                          )
-
-# This ensures that faster jobs finish earlier
-HYPERPARAM_GRID = list(HYPERPARAM_GRID)
-np.random.shuffle(HYPERPARAM_GRID)
-PRECOND_WARMUP = 100
-PRECOND_RESAMPLE = False
-PRECOND_ZSAMPLES = 1
+LOG_DIR = "logs10"
+HP_DICT = {
+    "T": (50,),
+    "seed": range(10),
+    # "dataset": ("covtype", "ijcnn1", "news20"),
+    "dataset": ("a9a", "w8a", "rcv1", "real-sim"),
+    "optimizer": ("SGD", "Adam", "SARAH", "L-SVRG"),
+    "corrupt": (None, (-3,0), (0,3), (-3,3)),
+    "BS": (128,),
+    "lr": (2**i for i in range(-16, 5, 2)),
+    "p": (0.99,),
+    "precond": (None, "hutchinson"),
+    "beta2": (0.999,),
+    "alpha": (1e-1, 1e-3, 1e-7),
+}
 
 
 def main():
@@ -55,55 +33,71 @@ def main():
     # Create log dirs on start up
     if not os.path.isdir(LOG_DIR):
         os.mkdir(LOG_DIR)
-    for dataset in DATASETS:
-        dataset_path = os.path.join(LOG_DIR, dataset)
-        if not os.path.isdir(dataset_path):
-            os.mkdir(dataset_path)
+    # Create folders for each dataset and for each corrupted version
+    dataset_paths = {}
+    for dataset in HP_DICT["dataset"]:
+        for corrupt in HP_DICT["corrupt"]:
+            if corrupt is None:
+                dataset_folder = dataset
+            else:
+                dataset_folder = f"{dataset}({corrupt[0]},{corrupt[1]})"
+            dataset_path = os.path.join(LOG_DIR, dataset_folder)
+            if not os.path.isdir(dataset_path):
+                os.mkdir(dataset_path)
+            # Add to paths
+            if dataset not in dataset_paths:
+                dataset_paths[dataset] = {}
+            dataset_paths[dataset][corrupt] = dataset_path
 
-    for seed, dataset, optimizer, BS, gamma, lam, p,\
-            precond, beta, alpha, corrupt in HYPERPARAM_GRID:
-        # Adam has its own preconditioning
-        if optimizer == "Adam":
-            precond = None
+    for hyperparams in product(*HP_DICT.values()):
+        hp = dict(zip(HP_DICT.keys(), hyperparams))
+
+        # Hard settings
+        if hp['optimizer'] in ("SGD", "Adam", "Adagrad", "Adadelta"):
+            hp['T'] *= 2
+
+        if 'lr_decay' not in hp:
+            hp['lr_decay'] = 0
+
+        if 'weight_decay' not in hp:
+            hp['weight_decay'] = 0
+
+        if hp['optimizer'] == "Adam":
+            hp['precond'] = None
+            hp['beta1'] = 0.9
+            hp['beta2'] = 0.999
+            hp['alpha'] = 1e-8
+
         # Create log file name in a way that remembers all args
-        args_str = f"seed={seed},BS={BS},gamma={gamma},lam={lam}"
-        if optimizer == "L-SVRG":
-            args_str += f",p={p}"
-        if precond is not None:
-            args_str += f",precond={precond}"
-        if precond == "hutchinson":
-            args_str += f",beta={beta},alpha={alpha}"
-        if corrupt is not None:
-            args_str += f",corrupt=[{corrupt[0]}-{corrupt[1]}]"
+        args_str = f"seed={hp['seed']}"
+        args_str += f",BS={hp['BS']}"
+        args_str += f",lr={hp['lr']}"
+        if hp['lr_decay'] != 0:
+            args_str += f",lr_decay={hp['lr_decay']}"
+        if hp['weight_decay'] != 0:
+            args_str += f",weight_decay={hp['weight_decay']}"
 
-        # log file is {LOG_DIR}/dataset/optimizer(arg1=val1,...,argN=valN).pkl
-        logfile = os.path.join(LOG_DIR, dataset, f"{optimizer}({args_str}).pkl")
+        if hp['optimizer'] in ("L-SVRG", "PAGE"):
+            args_str += f",p={hp['p']}"
+
+        if hp['precond'] == "hutchinson":
+            args_str += f",precond={hp['precond']}"
+            args_str += f",beta2={hp['beta2']}"
+            args_str += f",alpha={hp['alpha']}"
+
+        # log file is {LOG_DIR}/dataset[(k_min,k_max)]/optimizer(arg1=val1,...,argN=valN).pkl
+        dataset_path = dataset_paths[hp['dataset']][hp['corrupt']]
+        logfile = os.path.join(dataset_path, f"{hp['optimizer']}({args_str}).pkl")
 
         # Skip if another job already started on this
         if os.path.exists(logfile):
             continue
-        # Quickly touch the log file to reserve this job
+        # Quickly touch the log file to reserve this job!
         with open(logfile, "wb") as f:
             pickle.dump([], f)
 
         # Create arg namespace to pass to train
-        args = Namespace(alpha=alpha,
-                         BS=BS,
-                         beta=beta,
-                         corrupt=corrupt,
-                         dataset=dataset,
-                         T=T + T if optimizer in ("SGD", "Adam") else T,
-                         gamma=gamma,
-                         lam=lam,
-                         optimizer=optimizer,
-                         precond=precond,
-                         precond_warmup=PRECOND_WARMUP,
-                         precond_resample=PRECOND_RESAMPLE,
-                         precond_zsamples=PRECOND_ZSAMPLES,
-                         p=p,
-                         savedata=logfile,
-                         savefig=None,
-                         seed=seed)
+        args = parse_args(namespace=Namespace(savedata=logfile, savefig=None, **hp))
 
         # Run
         print(logfile)
