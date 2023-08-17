e8ce5e100d7a5f83589903b01635c0d651034c5d
Abdulla Almansoori <abdulla.almansoori@mbzuai.ac.ae>
2023-08-18 03:47:37 +0400

    .

--- src/job.sh
+++ src/job.sh
@@ -4,10 +4,5 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks=128
 ##SBATCH --time=02:00:00
-pwd; hostname; date
-source activate opt # assuming conda env 'opt' exists
-
 srun python src/run_experiment.py
-
-date
 
