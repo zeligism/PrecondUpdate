520814805f78b262d02bac206c83cc7482e8a569
Abdulla Almansoori <abdulla.almansoori@mbzuai.ac.ae>
2023-08-27 01:52:41 +0400

    .

--- src/pytorch/optim/scaled_optim.py
+++ src/pytorch/optim/scaled_optim.py
@@ -11,7 +11,7 @@ class ScaledOptimizer(optim.Optimizer):
             group.setdefault('alpha', alpha)
         self.global_state.setdefault('zsamples', zsamples)  # num of z samples in update_diagonal
         self.global_state.setdefault('warmup', warmup)  # num of diagonal warmup iters
-        self.global_state.setdefault('t', 0)  # num of step iters
+        self.global_state.setdefault('D_iters', 0)  # num of diagonal updates
         self.global_state.setdefault('layer_wise', layer_wise)
         self.global_state.setdefault('scaled_z', scaled_z)
 
@@ -22,7 +22,7 @@ class ScaledOptimizer(optim.Optimizer):
             group.setdefault('alpha', 1e-5)
         self.global_state.setdefault('zsamples', 1)  # num of z samples in update_diagonal
         self.global_state.setdefault('warmup', 1)  # num of diagonal warmup iters
-        self.global_state.setdefault('t', 0)  # num of step iters
+        self.global_state.setdefault('D_iters', 0)  # num of diagonal updates
         self.global_state.setdefault('layer_wise', True)
         self.global_state.setdefault('scaled_z', True)
 
@@ -38,7 +38,7 @@ class ScaledOptimizer(optim.Optimizer):
         Updates diagonal based on Hutchinson trace estimation.
         `closure(create_graph=True)` should be called right before calling this method.
         """
-        t = self.global_state['t']
+        D_iters = self.global_state['D_iters']
         warmup = self.global_state['warmup']
         layer_wise = self.global_state['layer_wise']
         scaled_z = self.global_state['scaled_z']
@@ -56,7 +56,7 @@ class ScaledOptimizer(optim.Optimizer):
                         for _ in range(zsamples):
                             z = torch.randint_like(p, 2) * 2 - 1
                             if scaled_z:
-                                scale = group['alpha'] if 'D' not in pstate else pstate['D'].abs().pow(0.5).add(group['alpha'])
+                                scale = group['alpha'] if 'D' not in pstate else pstate['D'].abs().add(group['alpha']).sqrt()
                             else:
                                 scale = 1
                             Hz, = torch.autograd.grad(p.grad, p, grad_outputs=z.div(scale), retain_graph=True)
@@ -83,7 +83,7 @@ class ScaledOptimizer(optim.Optimizer):
 
         # Apply scaled update
         for group in self.param_groups:
-            beta = 1 - 1 / (t + warmup) if group['beta'] in ("avg", "auto") else group['beta']
+            beta = 1 - 1 / (D_iters + warmup) if group['beta'] in ("avg", "auto") else group['beta']
             for p in group['params']:
                 pstate = self.state[p]
                 if p.grad is None:
@@ -94,7 +94,7 @@ class ScaledOptimizer(optim.Optimizer):
                     # Hutchinson: D = z * p.grad = z * d(df(w)/dw z)/dw = z * d^2f(w)/d^2w z = z * H z
                     D = pstate['z'].mul(p.grad)  # recall D = z o Hz, and p.grad holds Hz
                     if 'D_t' in pstate:
-                        D_diff += torch.norm(pstate['D_t'] - D)**2
+                        D_diff += torch.norm(pstate['D_t'] - D).pow(2)
                     pstate['D_t'] = D
                     pstate['z'] = None
                 # update diagonal
@@ -102,32 +102,34 @@ class ScaledOptimizer(optim.Optimizer):
                     if 'D' not in pstate:
                         pstate['D'] = D
                     else:
-                        pstate['D'].mul_(t).add_(D).div(t+1)
+                        pstate['D'].mul_(D_iters).add_(D).div(D_iters + 1)
                 else:
                     pstate['D'].mul_(beta).add_(D.mul(1 - beta))
-                D_sum += torch.norm(pstate['D'])**2
+                D_sum += torch.norm(pstate['D']).pow(2)
                 p.grad = None
+
+        self.global_state['D_iters'] += 1
 
 
     @torch.no_grad()
     def step(self, closure):
-        t = self.global_state['t']
+        D_iters = self.global_state['D_iters']
         warmup = self.global_state['warmup']
 
         closure = torch.enable_grad()(closure)  # always enable grad for closure
 
         ##### Warming up diagonal #####
-        if t < warmup:
-            if t == 0:
+        if D_iters < warmup:
+            if D_iters == 0:
                 print("Warming up...")
             loss = closure(create_graph=True)
             self.update_diagonal(init_phase=True)
-            self.global_state['t'] += 1
             return loss
-        elif t == warmup:
-            print("Warm up done.")
 
         ##### Update diagonal #####
+        elif D_iters == warmup:
+            print("Warm up done.")
+
         closure(create_graph=True)
         self.update_diagonal()
 
@@ -151,7 +153,6 @@ class ScaledOptimizer(optim.Optimizer):
                     D = pstate['D'].abs().clamp(min=alpha)
                     p.copy_(pstate['orig'].addcdiv(pstate['orig'].sub(p), D, value=-1))
 
-        self.global_state['t'] += 1
         return loss
 
 
