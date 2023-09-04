b1d0f6a4b643faed8b62e402e1c1ebc23dfeeeb8
Abdulla Almansoori <abdulla.almansoori@mbzuai.ac.ae>
2023-09-05 01:59:09 +0400

    .

--- src/pytorch/run_experiment.py
+++ src/pytorch/run_experiment.py
@@ -9,23 +9,20 @@ from train import *
 
 
 DRY_RUN = False  # for testing
-LOG_DIR = "logs_torch_test"
+LOG_DIR = "logs_torch"
 HP_DICT = {
     "epochs": (15,),
     "seed": range(3),
-    # "dataset": ("mnist", "cifar-10"),
-    "dataset": ("mnist",),
+    "dataset": ("mnist", "cifar-10"),
     "optimizer": ("Adam", "L-SVRG", "SARAH", "SGD"),
     "batch_size": (128,),
     "p": (0.999,),
     "lr": (2**-6, 2**-8, 2**-10, 2**-12),
     "precond": ("none", "hutchinson"),
-    "warmup": (40,),
+    "warmup": (50,),
     "zsamples": (10,),
-    # "beta1": (0.0, 0.9),
     "beta1": (0.0,),
     "beta2": ("avg", 0.999, 0.99),
-    # "alpha": (1e-1, 1e-3, 1e-7),
     "alpha": (1e-1, 1e-3),
 }
 
